1f4116deb3efb97ad7ed6f9cb4def6312caa2673
Bryan Mytko <bryanmytko@gmail.com>
2016-08-27 20:54:26 -0400

    Add function to remove chat UI; incomplete, some elements unknown

--- MinimalUI.lua
+++ MinimalUI.lua
@@ -1,20 +1,49 @@
-function Greeting(self)
-  local name = UnitName("player")
-  message("Hello " .. name .. realm)
-end
-
-Greeting()
+-- function Greeting(self)
+--   local name = UnitName("player")
+--   message("Hello " .. name)
+-- end
+--
+-- Greeting()
 
 -- Create a hidden parent to attach frames we want to hide to
 local HiddenUI = CreateFrame("Frame", "BlizzardUIHider", UIParent)
 HiddenUI:Hide()
 
 -- Hide default frames
-MainMenuBarArtFrame:SetParent(HiddenUI)
-MainMenuExpBar:SetParent(HiddenUI)
-MainMenuBarMaxLevelBar:SetParent(HiddenUI)
+-- MainMenuBarArtFrame:SetParent(HiddenUI)
+-- MainMenuExpBar:SetParent(HiddenUI)
+-- MainMenuBarMaxLevelBar:SetParent(HiddenUI)
 
--- Demo stuff
+-- Hide Chat Frame
+function hideStuff()
+  for i = 1, 10 do
+    _G[("FriendsMicroButton")]:Hide()
+    _G[("ChatFrame%d"):format(i)]:Hide()
+    _G[("ChatFrame%dTab"):format(i)]:Hide()
+    _G[("ChatFrame%dEditBox"):format(i)]:Hide()
+    _G[("ChatFrame%dEditBoxLeft"):format(i)]:Hide()
+    _G[("ChatFrame%dEditBoxRight"):format(i)]:Hide()
+    _G[("ChatFrame%dEditBoxMid"):format(i)]:Hide()
+    _G[("ChatFrame%dEditBoxActive"):format(i)]:Hide()
+    _G[("ChatFrame%dButtonFrameUpButton"):format(i)]:Hide()
+    _G[("ChatFrame%dButtonFrameDownButton"):format(i)]:Hide()
+    _G[("ChatFrame%dButtonFrameBottomButton"):format(i)]:Hide()
+    _G[("ChatFrame%dButtonFrame"):format(i)]:Hide()
+  end
+end
+
+hideStuff()
+DEFAULT_CHAT_FRAME:SetScript("OnShow", hideStuff)
+
+local start, duration, enable = GetActionCooldown(1);
+
+-- if (start == 0) then
+--   message('cooling')
+-- else
+--   message('ready')
+-- end
+
+-- Demo stuff to create frames
 
 -- local scale = 1.5
 --
