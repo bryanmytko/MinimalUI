a413fd69652cf0d4291a3798a6a0e25965e97b06
Bryan Mytko <bryanmytko@gmail.com>
2016-08-31 03:56:43 -0400

    Remove test code

--- ToyBox.lua
+++ ToyBox.lua
@@ -1,9 +1,6 @@
 SLASH_TOYBOX_RAFT1 = "/raft";
 SLASH_TOYBOX_MILLHOUSE1 = "/millhouse";
 
-local EventFrame = CreateFrame("Frame")
-EventFrame:RegisterEvent("PLAYER_LOGIN")
-
 function SlashCmdList.TOYBOX_RAFT(msg, editbox)
   ChatFrame1:AddMessage("/usetoy Cursed Orb")
 end
