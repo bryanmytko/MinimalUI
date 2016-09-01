f9b31f8fc2f0fed040de76586fdf24a9c096d656
Bryan Mytko <bryanmytko@gmail.com>
2016-09-01 03:55:44 -0400

    Add Manastorm Duplicator

--- ToyBox.lua
+++ ToyBox.lua
@@ -1,9 +1,13 @@
 SLASH_TOYBOX_RAFT1 = "/raft";
+SLASH_TOYBOX_MILLHOUSE1 = "/millhouse";
 
 local EventFrame = CreateFrame("Frame")
 EventFrame:RegisterEvent("PLAYER_LOGIN")
 
+function SlashCmdList.TOYBOX_RAFT(msg, editbox)
+  ChatFrame1:AddMessage("/usetoy Cursed Orb")
+end
 
 function SlashCmdList.TOYBOX_RAFT(msg, editbox)
-  ChatFrame1:AddMessage('/usetoy Cursed Orb')
+  ChatFrame1:AddMessage("/usetoy Manastorm's Duplicator")
 end
