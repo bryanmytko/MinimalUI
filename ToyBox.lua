120873685f741ff0b4d0a9cfb4b3d09796aead84
Bryan Mytko <bryanmytko@gmail.com>
2016-09-03 04:06:40 -0400

    Add Gamons Braid

--- ToyBox.lua
+++ ToyBox.lua
@@ -1,5 +1,6 @@
 SLASH_TOYBOX_RAFT1 = "/raft";
 SLASH_TOYBOX_MILLHOUSE1 = "/millhouse";
+SLASH_TOYBOX_GAMON1 = "/gamon";
 
 function SlashCmdList.TOYBOX_RAFT(msg, editbox)
   ChatFrame1:AddMessage("/usetoy Cursed Orb")
@@ -7,4 +8,8 @@ end
 
 function SlashCmdList.TOYBOX_RAFT(msg, editbox)
   ChatFrame1:AddMessage("/usetoy Manastorm's Duplicator")
+end
+
+function SlashCmdList.TOYBOX_GAMON(msg, editbox)
+  ChatFrame1:AddMessage("/usetoy Gamon's Braid")
 end
